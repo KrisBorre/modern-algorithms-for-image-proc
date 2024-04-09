266e2efe4a84b44a183c38b485d03983bccfa063
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-04-09 14:17:44 +0200

    CImage

--- Code/PulseNoiseWF/PulseNoiseWF/Queue.cs
+++ Code/PulseNoiseWF/PulseNoiseWF/Queue.cs
@@ -1,57 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace PulseNoiseWF
+﻿namespace PulseNoiseWF
 {
-  class Queue
-  { public
-     int input, output, Len, full;
-    public
-      int[] Array;
-    ~Queue() { }
-    public Queue(int len)  // Constructor
+    class Queue
     {
-      this.Len = len;
-      this.input = 0;
-      this.output = 0;
-      this.full = 0;
-      this.Array = new int[Len];
-    }
+        public int input, output, Len, full;
+        public int[] Array;
 
-    public int Put(int V)
-    {
-      if (input == Len - 1)
-      {
-          full = 1;
-          return -1;
-      }
-      Array[input] = V;
-      input++;
-      return 1;
-    }
+        ~Queue() { }
 
-    public int Get()
-    {
-        if (Empty() == 1)
+        public Queue(int len)  // Constructor
         {
-            return -1;
+            this.Len = len;
+            this.input = 0;
+            this.output = 0;
+            this.full = 0;
+            this.Array = new int[Len];
         }
-        int i = Array[output];
-        if (output == Len - 1) output = 0;
-        else output++;
-        if (full == 1) full = 0;
-        return i;
-    }
 
-    public int Empty()
-    {
-        if (input == output && full != 1)
+        public int Put(int V)
         {
+            if (input == Len - 1)
+            {
+                full = 1;
+                return -1;
+            }
+            Array[input] = V;
+            input++;
             return 1;
         }
-        return 0;
-    }
+
+        public int Get()
+        {
+            if (Empty() == 1)
+            {
+                return -1;
+            }
+            int i = Array[output];
+            if (output == Len - 1) output = 0;
+            else output++;
+            if (full == 1) full = 0;
+            return i;
+        }
+
+        public int Empty()
+        {
+            if (input == output && full != 1)
+            {
+                return 1;
+            }
+            return 0;
+        }
     } //************************ end class Queue *******************************
 }
